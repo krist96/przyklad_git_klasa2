1ba4b2487d46f56c5bfa54addba003f9ee776a30
Krystian <kristatx96@gmail.com>
2023-04-22 12:59:02 +0200

    testowyZapis 0675

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,6 @@ import Klasy.Osoba;
 
 public class Main {
     public static void main(String[] args) {
-        Osoba osoba = new Osoba("Adam", "Kowalski");
-        System.out.println(osoba.getInfo());
-        Osoba osoba2 = new Osoba("Amelia", "Nowak");
-        System.out.println(osoba2.getInfo());
-        Osoba osoba3 = new Osoba("Amelia", "Nowak");
-        System.out.println(osoba3.getInfo());
-        System.out.println(osoba3.getInfo());
         System.out.println("test 56456");
         System.out.println("Hello 5555555555");
     }
