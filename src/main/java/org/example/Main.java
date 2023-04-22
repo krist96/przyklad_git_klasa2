da202aae45fedd78eea665e54d0dd65714eeb66d
Krystian <kristatx96@gmail.com>
2023-04-22 11:18:25 +0200

    c

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,9 @@ import Klasy.Osoba;
 
 public class Main {
     public static void main(String[] args) {
-
         Osoba osoba = new Osoba("Adam", "Kowalski");
-        System.out.println("Imię: " + osoba.imie + "\n" + "Nazwisko:" +osoba.nazwisko);
+        System.out.println(osoba.getInfo());
         Osoba osoba2 = new Osoba("Amelia", "Nowak");
-        System.out.println("Imię: " + osoba2.imie + "\n" + "Nazwisko:" +osoba2.nazwisko);
+        System.out.println(osoba2.getInfo());
     }
 }
